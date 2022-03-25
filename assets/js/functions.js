c9750d8cda47cc51b2adba2b2c37a8290a6736e6
Jonas Paberzis <jonas.paberzis@gmail.com>
2022-03-25 19:10:44 +0100

    The "Western" Gods We Give Our Young

---
+++ assets/js/functions.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+// functions go here
+
+});
+
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  document.body.style.overflow = "hidden";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.overflow = "auto";
+}
+
+
+
+
+
+function openNav1() {
+  document.getElementById("mySidenav1").style.width = "100%";
+  document.body.style.overflow = "hidden";
+}
+
+function closeNav1() {
+  document.getElementById("mySidenav1").style.width = "0";
+  document.body.style.overflow = "auto";
+}
+
+
+
+function openNav2() {
+  document.getElementById("mySidenav2").style.width = "100%";
+  document.body.style.overflow = "hidden";
+}
+
+function closeNav2() {
+  document.getElementById("mySidenav2").style.width = "0";
+  document.body.style.overflow = "auto";
+}
+
+
+
+function openNav3() {
+  document.getElementById("mySidenav3").style.width = "100%";
+  document.body.style.overflow = "hidden";
+}
+
+function closeNav3() {
+  document.getElementById("mySidenav3").style.width = "0";
+  document.body.style.overflow = "auto";
+}
+
+
+
+function openNav4() {
+  document.getElementById("mySidenav4").style.width = "100%";
+  document.body.style.overflow = "hidden";
+}
+
+function closeNav4() {
+  document.getElementById("mySidenav4").style.width = "0";
+  document.body.style.overflow = "auto";
+}
